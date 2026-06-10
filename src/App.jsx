a1e73536fa62f28c315e9856ef78984794fc14e1
Pri-21-coder <prithapal114@gmail.com>
2026-06-10 22:02:27 +0530

    change file name

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Hero/hero";
+import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
 import Experience from "./components/Experience/Experience";
